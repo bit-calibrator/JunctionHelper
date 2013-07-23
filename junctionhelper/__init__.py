41d4f1f6356dbe725b1e787feb30d0093d84381c
Michael Gassman <mike@bitcalibrator.com>
2013-07-23 01:19:39 -0500

    Cleaning up, ready for first release

--- junctionhelper/__init__.py
+++ junctionhelper/__init__.py
@@ -1,2 +1,8 @@
-import helpers.py
+import os
+import shutil
+from ntfsutils import junction
 
+def move_and_junction(src,dest):
+    shutil.move(src,dest)
+    junction.create(dest, src) 
+    
--- junctionhelper/helpers.py
+++
@@ -1,7 +0,0 @@
-import os
-from ntfsutils import junction
-
-def move_and_junction(src,dest):
-    os.rename(src,dest)
-    junction.create(src,dest) 
-    
